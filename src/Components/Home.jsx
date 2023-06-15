71c3e9b38480ddf91dcd52d85af92ac92d8dc422
unknown <sahu.himanshu2016@gmail.com>
2023-06-16 00:15:19 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>jdhjfbsdf</h1>
+      <h1>Welcome to home page </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perspiciatis odio inventore cumque incidunt voluptate laudantium doloribus, quae sit tempore consequatur repellendus voluptas voluptates maxime beatae? Enim nisi molestiae veritatis alias quis fuga incidunt hic est quae ipsa? Fugiat illo quasi cum placeat error ducimus quas aspernatur reprehenderit minima mollitia!</p>
     </div>
   )
 }
